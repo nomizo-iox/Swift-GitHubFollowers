92e09a98c232811a3fb29fea4b7667d98e018923
nomizo-iox <nomizo.iox@gmail.com>
2020-01-05 20:46:01 -0600

    first commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Samuel F. Ademola on 1/5/20.
+//  Copyright © 2020 Nomizo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
